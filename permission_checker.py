200d84a94c808112e315fae444756856f45913fd
Hiroyuki Shirakawa <shirakawa.hiroyuki@velc.co.jp>
2020-03-17 06:14:32 +0000

    use csv than json

--- permission_checker.py
+++ permission_checker.py
@@ -1,4 +1,4 @@
-import json
+import csv
 import logging
 
 logger = logging.getLogger(__name__)
@@ -6,12 +6,13 @@ logger = logging.getLogger(__name__)
 
 class PermissionChecker:
     def __init__(self):
-        with open("permissions.json", "r") as f:
-            self.permissions = json.load(f)
+        with open("permissions.csv", "r") as f:
+            reader = csv.DictReader(f)
+            self.permissions = {row["idm"]: row["name"] for row in reader}
 
     def validate_idm(self, idm: str) -> bool:
         if idm not in self.permissions:
             logger.warning(f"Not Permitted: {idm}")
             return False
-        logger.info(f"Permitted: {idm}, for User: {self.permissions[idm]['name']}")
+        logger.info(f"Permitted: {idm}, for User: {self.permissions[idm]}")
         return True
